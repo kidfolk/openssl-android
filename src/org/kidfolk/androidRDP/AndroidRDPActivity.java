5ea79166a32ad5e105bc893922434916e1706ed3
nicky xu <kidfolk@gmail.com>
2011-10-29 15:36:46 +0800

    finish combining pic

--- src/org/kidfolk/androidRDP/AndroidRDPActivity.java
+++ src/org/kidfolk/androidRDP/AndroidRDPActivity.java
@@ -3,6 +3,7 @@ package org.kidfolk.androidRDP;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
@@ -51,7 +52,13 @@ public class AndroidRDPActivity extends Activity {
 		});
 		rdesktopMainThread.start();
 		cache_save_state();
-		
+
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		rdp_disconnect();
 	}
 
 	private native String getenv();
@@ -78,25 +85,59 @@ public class AndroidRDPActivity extends Activity {
 	private native void rdp_send_input(int time, short message_type,
 			short device_flags, short param1, short param2);
 
+	private void renderBitmap(int x, int y, int cx, int cy, int width,
+			int height, byte[] data) {
+		Log.v(TAG, "buffer position:" + buffer.position());
+		// count++;
+		// buffer.position(y*2);
+		ByteBuffer tmpBuffer = ByteBuffer.wrap(data);
+		Bitmap tmpBitmap = Bitmap.createBitmap(width, height, Config.RGB_565);
+		tmpBitmap.copyPixelsFromBuffer(tmpBuffer);
+		canvas.drawBitmap(tmpBitmap, x, y, null);
+//		canvas.save(Canvas.ALL_SAVE_FLAG);
+//		canvas.restore();
+		
+		runOnUiThread(new Runnable(){
 
-	private void renderBitmap( int x, int y, int cx, int cy,
-			int width, int height, byte[] data) {
-//		Log.v(TAG, "getBitmapBytesFormNative in java");
-//		Log.v(TAG, "x : " + x);
-//		Log.v(TAG, "y : " + y);
-//		Log.v(TAG, "width : " + width);
-//		Log.v(TAG, "height : " + height);
-//		Log.v(TAG, "data length : " + data.length);
+			@Override
+			public void run() {
+				imageView.setImageBitmap(bitmap);
+				
+			}
+			
+		});
 		
-		
-	    Bitmap bitmap = Bitmap.createBitmap(width, height, Config.RGB_565);
-		ByteBuffer buffer = ByteBuffer.wrap(data);
-		bitmap.copyPixelsFromBuffer(buffer);
-		
+
+//		File file = new File("/sdcard/" + new Date().hashCode() + ".png");
+//		OutputStream ops = null;
+//		try {
+//			ops = new FileOutputStream(file);
+//			bitmap.compress(CompressFormat.PNG, 100, ops);
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} finally {
+//			try {
+//				ops.close();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+
 	}
-	
+
+	private int count = 0;
+
 	private ImageView imageView;
+
+	private static ByteBuffer buffer = ByteBuffer.allocateDirect(800 * 600 * 4);
+
+	private static Bitmap bitmap = Bitmap
+			.createBitmap(800, 600, Config.RGB_565);
 	
+	private static Canvas canvas = new Canvas(bitmap);
+
 	private static final String TAG = "AndroidRDPActivity";
 
 	static {
